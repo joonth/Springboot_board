83acfb136f660509078e102cdf882c3a555ae286
joonth <joonthy@gmail.com>
2019-05-12 12:29:12 +0900

    회원가입 구현

---
+++ src/main/java/com/start/web/UserController.java
@@ -0,0 +1,15 @@
+package com.start.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+	
+	@PostMapping("/create")
+	public String create(User user) {
+		System.out.println("user  : " + user.toString());
+		return "index";
+	}
+}
