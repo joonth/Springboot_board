2aaca3de399d316f60f491efa78b057613f0316d
joonth <joonthy@gmail.com>
2019-05-17 23:13:53 +0900

    Login 기능 구현

--- src/main/java/com/start/domain/UserRepository.java
+++ src/main/java/com/start/domain/UserRepository.java
@@ -3,5 +3,5 @@ package com.start.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	User findByUserid(String userid);
 }
--- src/main/java/com/start/web/UserController.java
+++ src/main/java/com/start/web/UserController.java
@@ -2,12 +2,15 @@ package com.start.web;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.start.domain.User;
@@ -16,8 +19,32 @@ import com.start.domain.UserRepository;
 @Controller
 @RequestMapping("/users")
 public class UserController {
+	
 	@Autowired
 	private UserRepository userRepository;
+
+	@GetMapping("loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserid(userId);
+		if(user == null) {
+			System.out.println("id 실패");
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!password.equals(user.getPassword())) {
+			System.out.println("password 실패");
+			return "redirect:/users/loginForm";
+		}
+		
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
 	
 	@GetMapping("/form")
 	public String form() {
@@ -45,11 +72,13 @@ public class UserController {
 		return "/user/list";
 	}
 	
-	@PostMapping("/{id}")
+	@PutMapping("/{id}")
 	public String update(@PathVariable Long id, User updateUser) {
 		User user = userRepository.findById(id).get();
 		user.update(updateUser);
 		userRepository.save(user);
 		return "redirect:/users";
 	}
+
+	
 }
